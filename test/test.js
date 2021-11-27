3955343e955ed1b57ed48b4e0caa7990e84557cc
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-11-26 17:53:49 -0700

    Updated testing file

--- test/test.js
+++ test/test.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
-const { Client, Intents, Embed } = require('../index');
-const client = new Client(process.env.TESTTOKEN, [Intents.GUILDS, Intents.GUILD_MESSAGES, Intents.GUILD_MEMBERS]);
+const { Client, Embed, EmitTypes } = require('../index');
+const client = new Client(process.env.TESTTOKEN, {"oauth2CacheSelf": true});
 
 
-client.on(Client.READY, () => {
+client.on(EmitTypes.READY, async () => {
     console.log(`${client.user.tag} has logged in.`);
+    // console.log(await client.api('oauth2/applications/@me'));
 });
 
-client.on(Client.MESSAGE_CREATE, message => {
-    if (!message.author.bot) message.channel.send(new Embed().setTitle("Hello user").setAuthor(message.author.tag, message.author.avatarURL).setDescription("Hello user").setFooter("Hello hello hello").setTimestamp().setColor("green").setThumbnail(client.user.avatarURL));
+client.on(EmitTypes.MESSAGE_CREATE, message => {
+    if (!message.author.bot && message.content == "t!hello") message.channel.send(new Embed().setTitle("Hello user").setAuthor(message.author.tag, message.author.avatarURL).setDescription("Hello user").setFooter("Hello hello hello").setTimestamp().setColor("green").setThumbnail(client.user.avatarURL));
 });
+
+client.on(EmitTypes.MESSAGE_DELETE, message => {
+    console.log(message)
+});
+
+client.on(EmitTypes.CHANNEL_PINS_UPDATE, pins => console.log(pins));
 
 
 client.login();
