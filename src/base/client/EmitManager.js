07ca21857ddbd521ac588db28cf60664e3768f03
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-11-26 17:51:55 -0700

    Directly gets modified emit type from EmitTypes class instead of parsing

--- src/base/client/EmitManager.js
+++ src/base/client/EmitManager.js
@@ -1,15 +1,13 @@
 'use strict';
 
+const EmitTypes = require('./EmitTypes');
+
 class EmitManager {
     static async manage(data, client, callback) {
         const options = data.d;
         const modified = await callback(options);
-
-        let emitType = data.t.toLowerCase().split("_");
-        if (emitType.length > 1) emitType[1] = emitType[1][0].toUpperCase() + emitType[1].slice(1);
-        emitType = emitType.join("");
-
-        client.emit(emitType, modified);
+        
+        client.emit(EmitTypes[data.t], modified);
     }
 }
 
