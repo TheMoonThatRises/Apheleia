135c11a0f9e267c790fc368a96ddcbbdf1e8a060
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-11-27 12:32:01 -0700

    Added function to construct api messages + more compact functions

--- src/base/managers/ChannelManager.js
+++ src/base/managers/ChannelManager.js
@@ -1,47 +1,16 @@
 'use strict';
 
 const Manager = require("../Manager");
-const Embed = require("../message/Embed");
-const Message = require("../message/Message");
 const CacheArray = require("../modifiers/CacheArray");
+const Message = require("../message/Message");
 
 class ChannelManager extends Manager {
     constructor(channel, messageCacheSize, token) {
-        super(channel, token);
+        super(channel, token, `channels/${channel.id}/`);
 
         this.messageCache = new CacheArray(messageCacheSize);
-    }
 
-    async send(...message) {
-        let data = {
-            "content": "",
-            "embeds": [
-                
-            ],
-            message_reference: this.replyContent ?? null
-        }
-
-        if (typeof message == "object" && message.content) data = message;
-        else if (message instanceof Embed) data.embeds.push(message);
-        else {
-            for (const content of message) {
-                if (content instanceof Embed) data.embeds.push({...content});
-                else if (typeof content == "string") data.content += content + "\n";
-            }
-        }
-
-        const sendReq = {
-            method: 'post',
-            url: this.baseHTTPURL + `channels/${(!(this instanceof Message)) ? this.id : this.channel.id}/messages`,
-            headers: this.headers,
-            data: data
-        };
-
-        return await this.axios(sendReq)
-            .catch(err => {
-                if (err.response.data.retry_after) setTimeout(() => this.send(data), err.response.data.retry_after);
-                else throw new Error(`Unable to send message:\n\n${err}`);
-            });
+        this.send = async (...message) => await this.api('messages', await Message.constructMessage(...message), 'post');
     }
 }
 
--- src/base/message/Message.js
+++ src/base/message/Message.js
@@ -2,25 +2,49 @@
 
 const Manager = require("../Manager");
 const UserManager = require("../managers/UserManager");
+const Embed = require("./Embed");
 
 class Message extends Manager {
-    constructor(message, client, token) {
-        super(message, token);
+    static ReplyContent = class {
+        constructor(messageId, guildId, channelId) {
+            this.message_id = messageId;
+            this.guild_id = guildId;
+            this.channel_id = channelId;
+        }
+    }
+
+    constructor(message, client) {
+        super(message, client.token);
         
         this.guild = client.guilds.get(message.guild_id);
         this.channel = client.channels.get(message.channel_id);
         
         this.author = (this.guild.members.get(message.author.id)) ? this.guild.members.get(message.author.id) : new UserManager(this.author);
 
-        this.replyContent = {
-            message_id: this.id, 
-            guild_id: this.guild.id, 
-            channel_id: this.channel.id
-        };
+        this.replyContent = new Message.ReplyContent(this.id, this.guild.id, this.channel.id);
+
+        this.reply = (...message) => this.channel.send(...message, this.replyContent);
+        this.delete = () => this.channel.api(`messages/${this.id}`, {}, 'delete');
     }
 
-    async reply(message) {
-        return await this.channel.send.bind(this)(message);
+    static async constructMessage(...message) {
+        let data = {
+            "content": "",
+            "embeds": [],
+            message_reference: null
+        }
+
+        if (typeof message == "object" && message.content) data = message;
+        else if (message instanceof Embed) data.embeds.push(message);
+        else {
+            for (const content of message) {
+                if (content instanceof Embed) data.embeds.push({...content});
+                else if (typeof content == "string") data.content += content + "\n";
+                else if (content instanceof Message.ReplyContent) data.message_reference = content;
+            }
+        }
+
+        return data;
     }
 }
 
