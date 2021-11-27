3d3472a7c6a830ef09e626a6e2b8591f6ec0c511
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-11-26 17:48:58 -0700

    Moved EmitTypes to it's own class and added all EmitTypes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Base
 exports.Client = require('./src/base/client/Client');
 exports.Intents = require('./src/base/client/Intents');
+exports.EmitTypes = require('./src/base/client/EmitTypes');
 
 // Message Helpers
 exports.Embed = require('./src/base/message/Embed');
