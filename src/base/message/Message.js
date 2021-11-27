c40b4869cd4bd653b0599d9bcaee6c746ce714b3
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-11-26 21:03:43 -0700

    Allows bot to reply to messages

--- src/base/message/Message.js
+++ src/base/message/Message.js
@@ -4,13 +4,23 @@ const Manager = require("../Manager");
 const UserManager = require("../managers/UserManager");
 
 class Message extends Manager {
-    constructor(message, guild, channel, token) {
+    constructor(message, client, token) {
         super(message, token);
         
-        this.guild = guild;
-        this.channel = channel;
+        this.guild = client.guilds.get(message.guild_id);
+        this.channel = client.channels.get(message.channel_id);
         
-        this.author = (guild.members.get(message.author.id)) ? guild.members.get(message.author.id) : new UserManager(this.author);
+        this.author = (this.guild.members.get(message.author.id)) ? this.guild.members.get(message.author.id) : new UserManager(this.author);
+
+        this.replyContent = {
+            message_id: this.id, 
+            guild_id: this.guild.id, 
+            channel_id: this.channel.id
+        };
+    }
+
+    async reply(message) {
+        return await this.channel.send.bind(this)(message);
     }
 }
 
