783bd22249c1acdf8d61f5abae67de9cfab33aa6
mustafa <89872617+mustafaisnotsmart@users.noreply.github.com>
2022-02-04 13:22:58 -0700

    Please delete this

--- index.js
+++
@@ -1,7 +0,0 @@
-// Base
-exports.Client = require("./src/base/client/Client");
-exports.Intents = require("./src/base/client/Intents");
-exports.EmitTypes = require("./src/base/client/EmitTypes");
-
-// Message Helpers
-exports.Embed = require("./src/base/message/Embed");
